0598ff07a1c864aecb9aad26316c792f3b2c0067
sujitbidawe <bidawesujit@gmail.com>
2020-03-20 08:06:08 +0530

    added: code to generate singlet coin flip output and store them

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,36 @@
 #!/usr/local/bin/bash
 
-randOut=$(( RANDOM%2 ))
+heads=0
+tails=0
+totalFlips=0
 
-if [[ $randOut -eq 0 ]]
-then
-		echo "heads"
-else
-		echo "tails"
-fi
+declare -A flipDict
+
+read -p "how many times do you want to flip the coin: " repeatitions
+
+# loop to generte multiple singlet coin flip output
+for (( flipCount=1; flipCount<=repeatitions; flipCount++ ))
+do
+		randOut=$(( RANDOM%2 ))
+
+		if [[ $randOut -eq 0 ]]
+		then
+				(( heads++ ))
+				flipDict[flip$flipCount]="H"
+		else
+				(( tails++ ))
+				flipDict[flip$flipCount]="T"
+		fi
+done
+
+#loop to display flip number and its output 
+for key in "${!flipDict[@]}"
+do
+		echo "$key": "${flipDict[$key]}"
+done
+
+headsPercent=`echo "scale=2; ($heads/($repeatitions))*100" | bc`
+tailsPercent=`echo "scale=2; ($tails/($repeatitions))*100" | bc`
+
+echo "percentage of singlet heads: $headsPercent"
+echo "percentage of singlet tails: $tailsPercent"
